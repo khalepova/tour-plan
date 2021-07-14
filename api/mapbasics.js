14aa3a03852355558d40a9a45f87878c9d325304
khalepova <khalepova@gmail.com>
2021-07-14 23:08:13 +1000

    yandex

---
+++ api/mapbasics.js
@@ -0,0 +1,26 @@
+var myMap;
+
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
+
+function init() {
+  // Создание экземпляра карты и его привязка к контейнеру с
+  // заданным id ("map").
+  myMap = new ymaps.Map(
+    "map",
+    {
+      // При инициализации карты обязательно нужно указать
+      // её центр и коэффициент масштабирования.
+      center: [7.838114, 98.299123], // Москва
+      zoom: 15,
+    },
+    {
+      searchControlProvider: "yandex#search",
+    }
+  );
+
+  document.getElementById("destroyButton").onclick = function () {
+    // Для уничтожения используется метод destroy.
+    myMap.destroy();
+  };
+}
